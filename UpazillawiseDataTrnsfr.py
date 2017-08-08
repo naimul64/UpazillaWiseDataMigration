7f05e6fff31d7a0d125dd7e5fe19f0ecb353411a
naimul <naimul@surecash.net>
2017-08-08 16:40:57 +0600

    used my csv writer instead of mysql cmd

--- UpazillawiseDataTrnsfr.py
+++ UpazillawiseDataTrnsfr.py
@@ -5,7 +5,11 @@
 import sys
 import datetime
 import os
-import subprocess
+import MySQLdb
+import sys
+import os
+import csv
+import datetime
 
 
 ## important variable
@@ -23,6 +27,19 @@ export_folder_name = "EXPORT"
 
 
 ###########
+
+def get_src_db_connection():
+    return MySQLdb.connect(src_ip,
+                           src_user_name,
+                           src_pwd,
+                           src_schema)
+
+
+def get_dest_db_connection():
+    return MySQLdb.connect(dest_ip,
+                           dest_user_name,
+                           dest_pwd,
+                           dest_schema)
 
 
 def connect_to_db():
@@ -52,47 +69,72 @@ def create_file(file_with_path):
         os.utime(file_with_path, None)
 
 
-def export_upazilla(upazilla_list):
+def export_upazilla(upazilla_list, db_con):
     with open('ExportUpazilla.sql', 'r') as myfile:
         data = myfile.read()
     query = data % upazilla_list
-    print query
-    create_dir_if_not_exist(os.path.join(export_folder_name, "upazilla_" + time_stamp))
 
-    command = 'mysql -h %s -u %s -p -sN -e "%s" %s > %s' % (src_ip,
-                                                            src_user_name,
-                                                            query,
-                                                            src_schema,
-                                                            os.path.join(export_folder_name, "upazilla_" + time_stamp,
-                                                                         "upazilla"))
-    create_file(os.path.join(export_folder_name, "upazilla_" + time_stamp, "upazilla"))
-    os.system(command)
-    print "Upazilla exported".upper()
+    export(db_con=db_con, query=query, export_entity="upazilla")
+
+def import_upazilla(dest_db_con):
+    export_entity = "upazilla"
+    cursor = dest_db_con.cursor()
+    query = """
+        SELECT * FROM upazilla LIMIT 1
+    """
+    cursor.execute(query)
+    if len(cursor.fetchall()) > 1:
+        choise = raw_input('Clear upazilla table?(y/n): ')
+        if choise in ['y', 'Y']:
+            cursor.execute("TRUNCATE TABLE upazilla")
+            dest_db_con.commit()
+            print("Upazilla table cleared")
+        else:
+            return
+
+    csv_data = csv.reader(file(os.path.join(export_folder_name, export_entity + "_" + time_stamp, export_entity + ".csv")))
+
+    for row in csv_data:
+        cursor.execute(""" INSERT INTO %s """)
+
+
+    
 
 
 def create_export_dir():
     create_dir_if_not_exist(export_folder_name)
 
 
-def export(table_name, query):
-    dir_path = os.path.join(export_folder_name, table_name + "_" + time_stamp)
-    create_dir_if_not_exist(dir_path)
+def export(db_con, query, export_entity):
+    cursor = db_con.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    description = cursor.description
+    header = ()
+    for desc in description:
+        header = header + (desc[0],)
+    cursor.close()
 
-    command = 'mysql -h %s -u %s -p -sN -e "%s" %s > %s' % (src_ip,
-                                                            src_user_name,
-                                                            query,
-                                                            src_schema,
-                                                            os.path.join(dir_path, table_name))
-    create_file(os.path.join(dir_path, table_name))
-    os.system(command)
-    print (table_name + " exported").upper()
+    if not os.path.exists('export'):
+        os.makedirs(os.path.join(export_folder_name, export_entity + "_" + time_stamp))
+
+    fp = open(os.path.join(export_folder_name, export_entity + "_" + time_stamp, export_entity + ".csv"), 'wb')
+    myFile = csv.writer(fp)
+    myFile.writerow(header)
+    myFile.writerows(rows)
+    fp.close()
+
+    print "\n" + export_entity + " exported to CSV."
 
 
 def main():
     create_export_dir()
     check_args(sys.argv)
+    src_db_con = get_src_db_connection()
+    dest_db_con = get_dest_db_connection()
 
-    export_upazilla(sys.argv[2])
+    export_upazilla(sys.argv[2], src_db_con)
+    import_upazilla(dest_db_con)
 
 
 main()
